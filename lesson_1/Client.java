468d907999272af9e22aef77126745182eb1dd56
Zagryadskiy Dmitriy <zagryadskiy.dima@gmail.com>
2021-11-14 17:14:42 +0500

    Zagryadskiy_lesson1

---
+++ lesson_1/Client.java
@@ -0,0 +1,44 @@
+package lesson_1;
+
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+
+public class Client {
+
+    ByteBuffer buffer;
+
+
+    public static void main(String[] args) {
+
+        new Client().start();
+
+    }
+
+        public void start () {
+            System.out.println("New client started on thread " + Thread.currentThread().getName());
+            Scanner sc = new Scanner(System.in);
+            buffer = ByteBuffer.allocate(256);
+            try {
+                SocketChannel channel = SocketChannel.open(new InetSocketAddress("localhost", 9000));
+                while (true) {
+                    channel.write(buffer.wrap(String.format(
+                            sc.next(),
+                            LocalDateTime.now(),
+                            Thread.currentThread().getName()
+                    ).getBytes()));
+                    buffer.clear();
+                    channel.read(buffer);
+                    System.out.println("echo " + new String(buffer.array()).trim());
+                    buffer.clear();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
