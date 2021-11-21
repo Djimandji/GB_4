bb4ba22d3c382976c3665fccca9cf64e19009e53
Zagryadskiy Dmitriy <zagryadskiy.dima@gmail.com>
2021-11-21 18:06:00 +0500

    Zagryadskiy_lesson2_v.1

---
+++ Netty/src/main/java/EchoServerHandler.java
@@ -0,0 +1,17 @@
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+    public EchoServerHandler() {
+    }
+
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ctx.write(msg);
+        ctx.flush();
+    }
+
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
